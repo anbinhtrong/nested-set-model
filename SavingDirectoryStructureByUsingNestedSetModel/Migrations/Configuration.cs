611a2abb5f9a1def6b566c787c8c67d2393998ae
Tam Chieu <tam.chieu@newoceaninfosys.com>
2017-04-04 01:57:10 +0700

    update

--- SavingDirectoryStructureByUsingNestedSetModel/Migrations/Configuration.cs
+++ SavingDirectoryStructureByUsingNestedSetModel/Migrations/Configuration.cs
@@ -41,12 +41,12 @@ namespace SavingDirectoryStructureByUsingNestedSetModel.Migrations
                     var query = @"declare @procName varchar(500)
                                     declare cur cursor 
 
-                                    for select [name] from sys.objects where type in ('FN','TF') 
+                                    for select [name] from sys.objects where type = 'p'
                                     open cur
                                     fetch next from cur into @procName
                                     while @@fetch_status = 0
                                     begin
-                                        exec('drop function [' + @procName + ']')
+                                        exec('drop procedure [' + @procName + ']')
                                         fetch next from cur into @procName
                                     end
                                     close cur
--- SavingDirectoryStructureByUsingNestedSetModel/StoredProcedures/vw_lftrgt.sql
+++
@@ -1,1 +0,0 @@
-CREATE VIEW vw_lftrgt AS select DirectoryTreeMap.lft AS lft from DirectoryTreeMap union select DirectoryTreeMap.rgt AS rgt from DirectoryTreeMap;
